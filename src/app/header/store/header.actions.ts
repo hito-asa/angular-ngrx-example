2181144ca0c5c28b85584a7ce65a7b664fd3e4f5
小川 幸大 <>
2022-01-13 14:21:24 +0900

    addToCartを作った

--- src/app/header/store/header.actions.ts
+++ src/app/header/store/header.actions.ts
@@ -1,5 +1,9 @@
 import { createAction, props } from '@ngrx/store';
 
+export const incrementCart = createAction(
+  '[Header] Increment Cart'
+);
+
 export const addToCart = createAction(
   '[Header] Add to Cart'
 );
--- src/app/header/store/header.reducer.ts
+++ src/app/header/store/header.reducer.ts
@@ -1,17 +1,21 @@
+import { createEntityAdapter, EntityAdapter, EntityState } from '@ngrx/entity';
 import { Action, createReducer, on } from '@ngrx/store';
+import { Product } from 'src/app/products/models/product';
 import * as ShoppingCartActions from './header.actions';
 
 export const headerFeatureKey = 'header';
 
-export interface ShoppingCartState {
+export interface ShoppingCartState extends EntityState<Product> {
   cartCount: number;
 }
+export const adapter: EntityAdapter<Product> = createEntityAdapter<Product>();
 
-export const initialState: ShoppingCartState = {
-  cartCount: 0
-};
+export const initialState: ShoppingCartState =  adapter.getInitialState({
+  cartCount: 0,
+});
 
 export const reducer = createReducer(
   initialState,
-  on(ShoppingCartActions.addToCart, state => ({ cartCount: state.cartCount + 1}))
+  on(ShoppingCartActions.incrementCart, state => ({ ...state, cartCount: state.cartCount + 1})),
+  on(ShoppingCartActions.addToCart, state => ({ ...state, cartCount: state.cartCount + 1}))
 );
